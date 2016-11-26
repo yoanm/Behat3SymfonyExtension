498f5e002cf60acf90311b549ba47774aff72a84
Yoanm <yoanm@users.noreply.github.com>
2016-11-26 21:54:08 +0100

    Add RebootKernel service

---
+++ tests/Yoanm/Behat3SymfonyExtension/Subscriber/RebootKernelSubscriberTest.php
@@ -0,0 +1,50 @@
+<?php
+namespace Tests\Yoanm\Behat3SymfonyExtension\Subscriber;
+
+use Behat\Behat\EventDispatcher\Event\ExampleTested;
+use Behat\Behat\EventDispatcher\Event\ScenarioTested;
+use Prophecy\Prophecy\ObjectProphecy;
+use Yoanm\Behat3SymfonyExtension\Handler\KernelHandler;
+use Yoanm\Behat3SymfonyExtension\Subscriber\RebootKernelSubscriber;
+
+/**
+ * Class RebootKernelSubscriberTest
+ */
+class RebootKernelSubscriberTest extends \PHPUnit_Framework_TestCase
+{
+    /** @var KernelHandler|ObjectProphecy */
+    private $kernelHandler;
+    /** @var RebootKernelSubscriber */
+    private $subscriber;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setUp()
+    {
+        $this->kernelHandler = $this->prophesize(KernelHandler::class);
+
+        $this->subscriber = new RebootKernelSubscriber(
+            $this->kernelHandler->reveal()
+        );
+    }
+
+    public function testGetSubscribedEvents()
+    {
+        $this->assertSame(
+            [
+                ScenarioTested::AFTER => 'rebootKernel',
+                ExampleTested::AFTER => 'rebootKernel',
+            ],
+            RebootKernelSubscriber::getSubscribedEvents()
+        );
+    }
+
+    public function testRebootKernel()
+    {
+        $this->kernelHandler->rebootSfKernel()
+            ->shouldBeCalledTimes(1);
+
+        $this->subscriber->rebootKernel();
+    }
+}
