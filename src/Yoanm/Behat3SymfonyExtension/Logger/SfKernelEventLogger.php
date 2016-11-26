ad0888ee5452825c9da21be83f3d064e8e906735
Yoanm <yoanm@users.noreply.github.com>
2016-11-26 20:48:43 +0100

    Add SfKernelEventLogger

---
+++ src/Yoanm/Behat3SymfonyExtension/Logger/SfKernelEventLogger.php
@@ -0,0 +1,91 @@
+<?php
+namespace Yoanm\Behat3SymfonyExtension\Logger;
+
+use Monolog\Logger;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+/**
+ * This class will log on :
+ * - kernel.request event => log that a request has been handled
+ * - kernel.exception event => log that an exception has been thrown
+ *
+ * It's really usefull to understand what happens behind the scene when a behat step is executed
+ */
+class SfKernelEventLogger implements EventSubscriberInterface
+{
+    /** @var Logger */
+    private $logger;
+
+    /**
+     * @param Logger $logger
+     * @throws \Exception
+     */
+    public function __construct(Logger $logger)
+    {
+        $this->logger = $logger;
+    }
+
+    /**
+     * @return array
+     */
+    public static function getSubscribedEvents()
+    {
+        return array(
+            KernelEvents::REQUEST => 'onKernelRequest',
+            KernelEvents::EXCEPTION => 'onKernelException',
+        );
+    }
+
+    /**
+     * @param GetResponseEvent $event
+     */
+    public function onKernelRequest(GetResponseEvent $event)
+    {
+        $this->log(
+            '[REQUEST_HANDLED]',
+            array(
+                'type' => ($event->getRequestType() == HttpKernelInterface::MASTER_REQUEST
+                    ? 'Master'
+                    : 'Sub'
+                ),
+                'method' => $event->getRequest()->getMethod(),
+                'uri' => $event->getRequest()->getUri(),
+            )
+        );
+    }
+
+    /**
+     * @param GetResponseForExceptionEvent $event
+     */
+    public function onKernelException(GetResponseForExceptionEvent $event)
+    {
+        $this->log(
+            '[EXCEPTION_THROWN]',
+            array(
+                'message' => $event->getException()->getMessage(),
+            ),
+            Logger::CRITICAL
+        );
+    }
+
+    /**
+     * @param string $message
+     * @param array  $context
+     * @param int    $level
+     */
+    private function log($message, array $context = array(), $level = Logger::DEBUG)
+    {
+        $this->logger->addRecord(
+            $level,
+            sprintf(
+                '[SfKernelEventLogger] - %s',
+                $message
+            ),
+            $context
+        );
+    }
+}
