29613273796c37fce3e01843b1cc761c52685325
Yoanm <yoanm@users.noreply.github.com>
2016-11-26 22:04:48 +0100

    Add SfKernelLogger service

---
+++ src/Yoanm/Behat3SymfonyExtension/Subscriber/SfKernelLoggerSubscriber.php
@@ -0,0 +1,47 @@
+<?php
+namespace Yoanm\Behat3SymfonyExtension\Subscriber;
+
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Kernel;
+use Yoanm\Behat3SymfonyExtension\Event\KernelEvent;
+use Yoanm\Behat3SymfonyExtension\Logger\SfKernelEventLogger;
+
+/**
+ * Class SfKernelLoggerSubscriber
+ */
+class SfKernelLoggerSubscriber implements EventSubscriberInterface
+{
+    /** @var SfKernelEventLogger */
+    private $sfKernelEventLogger;
+
+    /**
+     * @param Kernel $kernel
+     */
+    public function __construct(SfKernelEventLogger $sfKernelEventLogger)
+    {
+        $this->sfKernelEventLogger = $sfKernelEventLogger;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getSubscribedEvents()
+    {
+        return [
+            KernelEvent::AFTER_BOOT => 'initSfKernelLogger',
+        ];
+    }
+
+
+    /**
+     * @param KernelEvent $event
+     */
+    public function initSfKernelLogger(KernelEvent $event)
+    {
+        /** @var EventDispatcherInterface $eventDispatcher the event dispatcher of SF application (not behat one)*/
+        $eventDispatcher = $event->getKernel()->getContainer()->get('event_dispatcher');
+
+        $eventDispatcher->addSubscriber($this->sfKernelEventLogger);
+    }
+}
